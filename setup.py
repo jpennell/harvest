a4da6c8271bea88f015cb0124554a5b3522dc410
James Pennell <pennell.james@gmail.com>
2012-08-19 14:10:44 -0400

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='harvest',
+    version='0.1',
+    author='James Pennell',
+    author_email='pennell.james@gmail.com',
+    packages=['harvest'],
+    scripts=[],
+    url='https://github.com/jpennell/harvest',
+    license='LICENSE.txt',
+    description='Text extraction framework',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
