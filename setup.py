8e844ec0faaca9f3ca872cf9a2e45303084d4a9b
James Pennell <pennell.james@gmail.com>
2012-08-24 12:24:40 -0400

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='harvest',
-    version='0.1',
+    version='0.2',
     author='James Pennell',
     author_email='pennell.james@gmail.com',
     packages=['harvest'],
